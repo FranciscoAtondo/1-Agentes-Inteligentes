74df4a2ccaa87d9945c7d223205ed74e0d1afaec
FranciscoAtondo <atondofrancisco@hotmail.com>
2025-01-21 13:45:22 -0700

    Actualizacion minima, aun no se prueba

--- tarea_1.py
+++ tarea_1.py
@@ -19,23 +19,24 @@ import entornos_o
 class NueveCuartos(entornos_o.Entorno):
     """
     Crea un entorno de nueve cuartos 3x3, osea:
-    #|#|#
-    #|#|#
-    #|#|#
+    A1|A2|A3
+    B1|B2|B3
+    C1|C2|C3
     
     Metodo Orientado a Objetos
     """
-    def __init__(self, x0=["A", "sucio", "sucio"]):
+    def __init__(self, x0=["C1", [["sucio", "sucio","sucio"], ["sucio", "sucio","sucio"], ["sucio", "sucio","sucio"]]]):
         """
-        Por default inicialmente el robot está en A y los dos cuartos
-        están sucios
+        Por default inicialmente el robot está en C1 y todos los cuartos
+        están sucios.
 
         """
         self.x = x0[:]
         self.costo = 0
 
     def accion_legal(self, accion):
-        return accion in ("ir_A", "ir_B", "limpiar", "nada")
+        return accion in ("ir_Derecha", "ir_Izquierda", "subir", "bajar", "limpiar", "nada")
+    #"ir_A", "ir_B", "limpiar", "nada"
 
     def transicion(self, accion):
         if not self.acción_legal(accion):
@@ -45,6 +46,7 @@ class NueveCuartos(entornos_o.Entorno):
         if accion != "nada" or a == "sucio" or b == "sucio":
             self.costo += 1
         if accion == "limpiar":
+            
             self.x[" AB".find(self.x[0])] = "limpio"
         elif accion == "ir_A":
             self.x[0] = "A"
